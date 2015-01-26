b27a777f70fee344560e6bd5f8a8a35561f96fa1
jgf218 <jgf218@lehigh.edu>
2015-01-26 05:03:10 +0000

    Finished hw01. Solved problem of slashes by adding a second

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//Joe Freeman
+//hw01
+//Welcome Class java Program
+//
+
+
+//Define a class
+public class WelcomeClass{
+    
+    //add a main method
+    public static void main(String[] args) {
+        //print out the statement 
+        System.out.println("-------------");
+        System.out.println("|  WELCOME  |");
+        System.out.println("-------------");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<--J--G--F--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("This summer, my family bought Kayaks and paddled around Cape Cod Bay");
+        
+    }
+}
