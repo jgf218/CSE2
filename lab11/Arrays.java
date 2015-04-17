fee78ff9d4b448462fb13698987f105aa53e02d3
jgf218 <jgf218@lehigh.edu>
2015-04-17 00:22:41 +0000

    lab 11 completed

--- lab11/Arrays.java
+++ lab11/Arrays.java
@@ -21,13 +21,28 @@ for(int c = 1; c<50; c++){
     int num = (int) ((Math.random() *100 )+ 1);
     array2[c] = array2[c-1] + num;
 }
- System.out.println("Enter an int: ");
- int Val = scan.nextInt();
-if(Val<0){
-    return;
-}
-    System.out.println("the maximum of array1 is: " +maxval(array1));
-System.out.println("the minimum of array1 is: " +minval(array1));
+System.out.println("the maximum of array2 is: " +maxval(array2));
+System.out.println("the minimum of array2 is: " +minval(array2));
+System.out.println("Enter an int: ");
+if(scan.hasNextInt()){
+    int num = 0;
+    num = scan.nextInt();
+    if (num>=0){
+      binarysearch(array2, num);
+    }
+          
+    else{
+    System.out.println("sorry please enter an integer greater than 0");
+    return;    
+        }
+        
+    }
+else{
+  System.out.println("sorry please enter an integer value");
+  return;
+    }
+
+   
 
 }
 
@@ -56,5 +71,41 @@ public static int minval(int[] array) {
       m=i;
 }
   return array[m];
+}
+public static void binarysearch(int array[], int val){
+    int Start = 0;
+    int End = 0;
+    int length = array.length;
+    int c = 1;
+if (val< array[0]){
+  System.out.println( "the number above  your target is "  + array[0]);
+         return;
+    }
+if (val> array[0]){
+  System.out.println( "the number below  your target is "  + array[array.length]);
+         return;
+    
+}
+while (true){
+    length /= 2;
+    if (array[length] == val){
+        System.out.println( val + " was found in the list");
+        return;
+    }
+    else if (array[length]< val){
+         Start = length;
+         End = array.length;
+        }
+    else{
+         Start = 0;
+         End = length;
+    }
+    length = (Start + End);
+    c++;
+    if (c>array.length/2 + 1){
+         System.out.println( "the numbers above and  below your target are "  + array[length] + " and " +array[length-1]);
+         return;
+    }
+} 
 }
 }
